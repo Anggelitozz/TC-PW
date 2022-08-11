4eed2abd4d335af16d2f0b8a819a08504db2d4c9
Anggelitozz <98678182+Anggelitozz@users.noreply.github.com>
2022-08-11 06:57:07 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const lista = document.getElementById("lista");
-
-function add(){
-    let texto = prompt("ingrese texto")
-    var item = document.createElement("li")
-    item.appendChild(document.createTextNode(texto))
-    lista.appendChild(item)
-}
-
